14b68bae5470242875bc216a9d98a04af7cbae30
WafaaAjan <ou_ajan@hotmail.com>
2021-12-20 21:08:47 +0100

    rettelse i CandidateRepository

--- src/main/java/com/example/kommunevalg/repository/CandidateRepository.java
+++ src/main/java/com/example/kommunevalg/repository/CandidateRepository.java
@@ -3,7 +3,6 @@ package com.example.kommunevalg.repository;
 import com.example.kommunevalg.model.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
