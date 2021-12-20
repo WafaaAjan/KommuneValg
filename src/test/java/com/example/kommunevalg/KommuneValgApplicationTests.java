b9edd934f28d985f0506d4eec71b027e1f07b660
WafaaAjan <ou_ajan@hotmail.com>
2021-12-20 11:26:07 +0100

    First commit

---
+++ src/test/java/com/example/kommunevalg/KommuneValgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kommunevalg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommuneValgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
